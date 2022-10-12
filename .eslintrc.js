6977fc6bf563fd8088306753f3547111ad2f82fb
Raj Soni <rajsoni19619@gmail.com>
2022-10-13 00:23:32 +0530

    [setup]: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        ecmaFeatures: {
+            jsx: true,
+        },
+        project: ['./tsconfig.json'],
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started with Create React App
+
+This project is to provide a stage for all the individuals to look out for their ideal life partner in Maidh Kshatriya Swarnakar community
