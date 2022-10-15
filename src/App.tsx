5feb78df1735cfafd9a88f1702c0bd59104477d8
Raj Soni <rajsoni19619@gmail.com>
2022-10-15 22:22:21 +0530

    [style]: update basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import './App.css';
-import React from 'react';
+import Header from './Components/Commons/Header/Header';
+import HomePage from './Pages/Homepage/Homepage';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Header />
+            <main>
+                <HomePage />
+            </main>
+        </>
     );
-}
+};
 
 export default App;
