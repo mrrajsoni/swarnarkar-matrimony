a51f9964642cc8c50ca972af0dee7e0326932aa4
Raj Soni <rajsoni19619@gmail.com>
2022-10-15 09:35:09 +0530

    [fix]: update css to sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.sass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
