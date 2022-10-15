200bfc82b386c9d82d92f8c02a55675bd17d7bd7
Raj Soni <rajsoni19619@gmail.com>
2022-10-15 22:20:34 +0530

    [chore]: revert jsx scope rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     },
     plugins: ['react', '@typescript-eslint'],
     rules: {
+        'react/react-in-jsx-scope': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-explicit-any': 'warn',
         '@typescript-eslint/no-unsafe-call': 'off',
