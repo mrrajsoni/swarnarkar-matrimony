d54da8074e32de724af37e331ef3bc7e4ef57661
Raj Soni <rajsoni19619@gmail.com>
2022-10-25 11:42:06 +0530

    [chore]: added formik, updated eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-explicit-any': 'warn',
         '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
         'prettier/prettier': [
             'error',
             {
