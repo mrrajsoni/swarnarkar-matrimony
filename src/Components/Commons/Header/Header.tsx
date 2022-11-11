3278f4d30b7df97ffd1e1c54cb40b9bb43f2d776
Raj Soni <rajsoni19619@gmail.com>
2022-11-12 01:08:10 +0530

    [fix]: added link to home for logo

--- src/Components/Commons/Header/Header.tsx
+++ src/Components/Commons/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import './Header.scss';
 
@@ -17,11 +18,13 @@ const Header = () => {
 const Logo = () => {
     return (
         <div className="logo-wrapper">
-            <img
-                className="logo"
-                src="https://res.cloudinary.com/rajsoni/image/upload/v1665834455/swarnakar_matrimonial_logo_ryofgv.png"
-                alt="Swarnakar matrimonial logo"
-            />
+            <Link to={'/'}>
+                <img
+                    className="logo"
+                    src="https://res.cloudinary.com/rajsoni/image/upload/v1665834455/swarnakar_matrimonial_logo_ryofgv.png"
+                    alt="Swarnakar matrimonial logo"
+                />
+            </Link>
         </div>
     );
 };
