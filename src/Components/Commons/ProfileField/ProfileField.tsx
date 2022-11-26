acec19f538cad81f9ad9a3314ad88f0f9b08d45a
Raj Soni <rajsoni19619@gmail.com>
2022-11-26 18:17:29 +0530

    [feat]: added classnames package

--- src/Components/Commons/ProfileField/ProfileField.tsx
+++ src/Components/Commons/ProfileField/ProfileField.tsx
@@ -1,6 +1,9 @@
+import classNames from 'classnames';
+
 const ProfileField = ({ label, field }: { label: string; field: string }) => {
+    const customClass = classNames({ empty: field === 'Not filled in' });
     return (
-        <li className="profile-field mb-2">
+        <li className={`profile-field mb-2 ${customClass}`}>
             <p className="profile-label">{label}</p>
             {field}
         </li>
