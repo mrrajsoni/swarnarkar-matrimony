225ca5c3bde5c14413b8fb546aa433085e2a8fb5
Raj Soni <rajsoni19619@gmail.com>
2022-12-13 16:02:34 +0530

    [chore]: added react query package

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import UserProvider from './Context/UserContext';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 10,
+        },
+    },
+});
 root.render(
     <UserProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+            <ReactQueryDevtools position="bottom-right" />
+        </QueryClientProvider>
     </UserProvider>,
 );
