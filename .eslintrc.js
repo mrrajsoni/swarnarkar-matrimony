7ab093b4f86de47455f10dda15118365c3c84a28
Raj Soni <rajsoni19619@gmail.com>
2022-12-15 18:04:12 +0530

    [chore]: fix eslint not working

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
         es2021: true,
         node: true,
     },
+    ignorePatterns: ['/*.*'],
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
